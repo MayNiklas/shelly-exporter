e3dfa83c1b431d868ad97439cbbd2cbe544dd025
MayNiklas <info@niklas-steffen.de>
2022-05-28 13:22:41 +0200

    add exporter example from https://github.com/prometheus/client_python

--- src/shelly_exporter/__main__.py
+++ src/shelly_exporter/__main__.py
@@ -1,9 +1,23 @@
 # -*- coding: utf-8 -*-
+import random
+import time
+
 from prometheus_client import start_http_server, Summary
 
-def main():
-    print("hello world!")
+# Create a metric to track time spent and requests made.
+REQUEST_TIME = Summary('request_processing_seconds', 'Time spent processing request')
+
+
+# Decorate function with metric.
+@REQUEST_TIME.time()
+def process_request(t):
+    """A dummy function that takes some time."""
+    time.sleep(t)
 
 
 if __name__ == '__main__':
-    main()
+    # Start up the server to expose the metrics.
+    start_http_server(8000)
+    # Generate some requests.
+    while True:
+        process_request(random.random())
