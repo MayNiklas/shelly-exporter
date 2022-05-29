257ddab3b3d06796aeba92c37f6cb3ec7726ae75
MayNiklas <info@niklas-steffen.de>
2022-05-29 20:38:52 +0200

    use local vars

--- cmd/shelly_exporter/main.go
+++ cmd/shelly_exporter/main.go
@@ -75,31 +75,6 @@ type shelly_data struct {
 
 var (
 	addr = flag.String("listen-address", ":8080", "The address to listen on for HTTP requests.")
-
-	shelly_power_current = prometheus.NewGauge(prometheus.GaugeOpts{
-		Name: "shelly_power_current",
-		Help: "Current power consumption of shelly.",
-	})
-
-	shelly_power_total = prometheus.NewGauge(prometheus.GaugeOpts{
-		Name: "shelly_power_total",
-		Help: "Total power consumption of shelly.",
-	})
-
-	shelly_uptime = prometheus.NewGauge(prometheus.GaugeOpts{
-		Name: "shelly_uptime",
-		Help: "Uptime of shelly.",
-	})
-
-	shelly_temperature = prometheus.NewGauge(prometheus.GaugeOpts{
-		Name: "shelly_temperature",
-		Help: "Temperature of shelly.",
-	})
-
-	shelly_update_available = prometheus.NewGauge(prometheus.GaugeOpts{
-		Name: "shelly_update_available",
-		Help: "OTA update is available.",
-	})
 )
 
 func Run() {
@@ -113,7 +88,52 @@ func Run() {
 	log.Fatal(http.ListenAndServe(*addr, nil))
 }
 
+// getShellyData returns the data from the shelly device
+func getShellyData(shelly_ip string) shelly_data {
+	resp, err := http.Get("http://" + shelly_ip + "/status")
+
+	if err != nil {
+		fmt.Println("No response from request")
+	}
+
+	defer resp.Body.Close()
+
+	// response body is []byte
+	body, err := ioutil.ReadAll(resp.Body)
+	var result shelly_data
+
+	// Parse []byte to the go struct pointer
+	if err := json.Unmarshal(body, &result); err != nil {
+		fmt.Println("Can not unmarshal JSON")
+	}
+
+	return result
+}
+
 func probeHandler(w http.ResponseWriter, r *http.Request) {
+
+	var (
+		shelly_power_current = prometheus.NewGauge(prometheus.GaugeOpts{
+			Name: "shelly_power_current",
+			Help: "Current power consumption of shelly.",
+		})
+		shelly_power_total = prometheus.NewGauge(prometheus.GaugeOpts{
+			Name: "shelly_power_total",
+			Help: "Total power consumption of shelly.",
+		})
+		shelly_uptime = prometheus.NewGauge(prometheus.GaugeOpts{
+			Name: "shelly_uptime",
+			Help: "Uptime of shelly.",
+		})
+		shelly_temperature = prometheus.NewGauge(prometheus.GaugeOpts{
+			Name: "shelly_temperature",
+			Help: "Temperature of shelly.",
+		})
+		shelly_update_available = prometheus.NewGauge(prometheus.GaugeOpts{
+			Name: "shelly_update_available",
+			Help: "OTA update is available.",
+		})
+	)
 
 	ctx, cancel := context.WithCancel(r.Context())
 	defer cancel()
@@ -162,26 +182,4 @@ func probeHandler(w http.ResponseWriter, r *http.Request) {
 	h := promhttp.HandlerFor(registry, promhttp.HandlerOpts{})
 	h.ServeHTTP(w, r)
 
-}
-
-// getShellyData returns the data from the shelly device
-func getShellyData(shelly_ip string) shelly_data {
-	resp, err := http.Get("http://" + shelly_ip + "/status")
-
-	if err != nil {
-		fmt.Println("No response from request")
-	}
-
-	defer resp.Body.Close()
-
-	// response body is []byte
-	body, err := ioutil.ReadAll(resp.Body)
-	var result shelly_data
-
-	// Parse []byte to the go struct pointer
-	if err := json.Unmarshal(body, &result); err != nil {
-		fmt.Println("Can not unmarshal JSON")
-	}
-
-	return result
 }
