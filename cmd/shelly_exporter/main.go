8ec1e45b4f8326c12d86d8f9586146abd412124b
MayNiklas <info@niklas-steffen.de>
2022-05-30 11:49:46 +0200

    get shelly_hostname

--- cmd/shelly_exporter/main.go
+++ cmd/shelly_exporter/main.go
@@ -8,6 +8,7 @@ import (
 	"io/ioutil"
 	"log"
 	"net/http"
+	"time"
 
 	"github.com/prometheus/client_golang/prometheus"
 	"github.com/prometheus/client_golang/prometheus/promhttp"
@@ -73,6 +74,118 @@ type shelly_data struct {
 	Uptime   int `json:"uptime"`
 }
 
+type shelly_settings struct {
+	Device struct {
+		Type       string `json:"type"`
+		Mac        string `json:"mac"`
+		Hostname   string `json:"hostname"`
+		NumOutputs int    `json:"num_outputs"`
+		NumMeters  int    `json:"num_meters"`
+	} `json:"device"`
+	WifiAp struct {
+		Enabled bool   `json:"enabled"`
+		Ssid    string `json:"ssid"`
+		Key     string `json:"key"`
+	} `json:"wifi_ap"`
+	WifiSta struct {
+		Enabled    bool        `json:"enabled"`
+		Ssid       string      `json:"ssid"`
+		Ipv4Method string      `json:"ipv4_method"`
+		IP         interface{} `json:"ip"`
+		Gw         interface{} `json:"gw"`
+		Mask       interface{} `json:"mask"`
+		DNS        interface{} `json:"dns"`
+	} `json:"wifi_sta"`
+	WifiSta1 struct {
+		Enabled    bool        `json:"enabled"`
+		Ssid       interface{} `json:"ssid"`
+		Ipv4Method string      `json:"ipv4_method"`
+		IP         interface{} `json:"ip"`
+		Gw         interface{} `json:"gw"`
+		Mask       interface{} `json:"mask"`
+		DNS        interface{} `json:"dns"`
+	} `json:"wifi_sta1"`
+	ApRoaming struct {
+		Enabled   bool `json:"enabled"`
+		Threshold int  `json:"threshold"`
+	} `json:"ap_roaming"`
+	Mqtt struct {
+		Enable              bool    `json:"enable"`
+		Server              string  `json:"server"`
+		User                string  `json:"user"`
+		ID                  string  `json:"id"`
+		ReconnectTimeoutMax float64 `json:"reconnect_timeout_max"`
+		ReconnectTimeoutMin float64 `json:"reconnect_timeout_min"`
+		CleanSession        bool    `json:"clean_session"`
+		KeepAlive           int     `json:"keep_alive"`
+		MaxQos              int     `json:"max_qos"`
+		Retain              bool    `json:"retain"`
+		UpdatePeriod        int     `json:"update_period"`
+	} `json:"mqtt"`
+	Coiot struct {
+		Enabled      bool   `json:"enabled"`
+		UpdatePeriod int    `json:"update_period"`
+		Peer         string `json:"peer"`
+	} `json:"coiot"`
+	Sntp struct {
+		Server  string `json:"server"`
+		Enabled bool   `json:"enabled"`
+	} `json:"sntp"`
+	Login struct {
+		Enabled     bool   `json:"enabled"`
+		Unprotected bool   `json:"unprotected"`
+		Username    string `json:"username"`
+	} `json:"login"`
+	PinCode      string `json:"pin_code"`
+	Name         string `json:"name"`
+	Fw           string `json:"fw"`
+	Discoverable bool   `json:"discoverable"`
+	BuildInfo    struct {
+		BuildID        string    `json:"build_id"`
+		BuildTimestamp time.Time `json:"build_timestamp"`
+		BuildVersion   string    `json:"build_version"`
+	} `json:"build_info"`
+	Cloud struct {
+		Enabled   bool `json:"enabled"`
+		Connected bool `json:"connected"`
+	} `json:"cloud"`
+	Timezone         string  `json:"timezone"`
+	Lat              float64 `json:"lat"`
+	Lng              float64 `json:"lng"`
+	Tzautodetect     bool    `json:"tzautodetect"`
+	TzUtcOffset      int     `json:"tz_utc_offset"`
+	TzDst            bool    `json:"tz_dst"`
+	TzDstAuto        bool    `json:"tz_dst_auto"`
+	Time             string  `json:"time"`
+	Unixtime         int     `json:"unixtime"`
+	LedStatusDisable bool    `json:"led_status_disable"`
+	DebugEnable      bool    `json:"debug_enable"`
+	AllowCrossOrigin bool    `json:"allow_cross_origin"`
+	Actions          struct {
+		Active bool     `json:"active"`
+		Names  []string `json:"names"`
+	} `json:"actions"`
+	Hwinfo struct {
+		HwRevision string `json:"hw_revision"`
+		BatchID    int    `json:"batch_id"`
+	} `json:"hwinfo"`
+	MaxPower        int  `json:"max_power"`
+	LedPowerDisable bool `json:"led_power_disable"`
+	Relays          []struct {
+		Name          interface{}   `json:"name"`
+		ApplianceType string        `json:"appliance_type"`
+		Ison          bool          `json:"ison"`
+		HasTimer      bool          `json:"has_timer"`
+		DefaultState  string        `json:"default_state"`
+		AutoOn        float64       `json:"auto_on"`
+		AutoOff       float64       `json:"auto_off"`
+		Schedule      bool          `json:"schedule"`
+		ScheduleRules []interface{} `json:"schedule_rules"`
+		MaxPower      int           `json:"max_power"`
+	} `json:"relays"`
+	EcoModeEnabled bool `json:"eco_mode_enabled"`
+}
+
 var (
 	port   = flag.String("port", "8080", "The port to listen on for HTTP requests.")
 	listen = flag.String("listen", "localhost", "The address to listen on for HTTP requests.")
@@ -105,6 +218,27 @@ func getShellyData(shelly_ip string) shelly_data {
 	// response body is []byte
 	body, err := ioutil.ReadAll(resp.Body)
 	var result shelly_data
+
+	// Parse []byte to the go struct pointer
+	if err := json.Unmarshal(body, &result); err != nil {
+		fmt.Println("Can not unmarshal JSON")
+	}
+
+	return result
+}
+
+func getShellySettings(shelly_ip string) shelly_settings {
+	resp, err := http.Get("http://" + shelly_ip + "/settings")
+
+	if err != nil {
+		fmt.Println("No response from request")
+	}
+
+	defer resp.Body.Close()
+
+	// response body is []byte
+	body, err := ioutil.ReadAll(resp.Body)
+	var result shelly_settings
 
 	// Parse []byte to the go struct pointer
 	if err := json.Unmarshal(body, &result); err != nil {
@@ -164,24 +298,23 @@ func probeHandler(w http.ResponseWriter, r *http.Request) {
 	fmt.Println("Probing: ", target)
 	var data shelly_data = getShellyData(target)
 
+	// get hostname of Shelly
+	// to do:
+	// return hostname as label to prometheus
+	var shelly_hostname string = getShellySettings(target).Device.Hostname
+	println(shelly_hostname)
+
 	// set metrics
-
 	shelly_power_current.Set(data.Meters[0].Power)
-
-	// // shelly_power_total.Set(result.Meters[0].Total) did not work
-	// // I'm not 100% sure of the implications of this, but it seems to work.
 	shelly_power_total.Set(float64(data.Meters[0].Total))
-
 	shelly_temperature.Set(data.Temperature)
-
+	shelly_uptime.Set(float64(data.Uptime))
 	// check if update is available
 	if data.Update.HasUpdate {
 		shelly_update_available.Set(1)
 	} else {
 		shelly_update_available.Set(0)
 	}
-
-	shelly_uptime.Set(float64(data.Uptime))
 
 	h := promhttp.HandlerFor(registry, promhttp.HandlerOpts{})
 	h.ServeHTTP(w, r)
